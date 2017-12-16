2fd46edc6c6b6bc78f50a1454a4fba4a56154089
Parker <parker.smith725@gmail.com>
2017-12-16 14:13:11 -0700

    adding test framework

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ app.use(bodyParser.json());
 
 //Routes
 app.post('/todos', (req,res) => {
-    console.log(req.body);
     var todo = new Todo({
         text:req.body.text
     });
@@ -27,3 +26,5 @@ app.post('/todos', (req,res) => {
 app.listen(3000, ()=> {
     console.log('Started on port 3000');
 });
+
+module.exports = {app};
