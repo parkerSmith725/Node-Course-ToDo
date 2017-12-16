36c1d101daf4d8839b23ef14349a9668825c3bc5
Parker <parker.smith725@gmail.com>
2017-12-16 16:05:19 -0700

    updated tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const testTodos = [{
+    text: 'First test todo'
+},{
+    text: 'Second test todo'
+}];
 
 beforeEach((done) => {
     Todo.remove({}).then(() => {
+        return Todo.insertMany(testTodos);
+    }).then(() => {
         done();
     });
 });
@@ -27,7 +34,7 @@ describe('POST /todos',() => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -49,11 +56,23 @@ describe('POST /todos',() => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => {
                     done(e);
                 });
             });
+    });
+});
+
+describe('GET /todos',() => {
+    it('should return all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
     });
 });
